ba8dbe5a9aa21ae92407f1f1baabdc1ff0a273ad
Stephen Eyring <accounts@eyring-s.de>
2024-12-08 12:40:12 +0100

    hyprlock

--- hyprland/.config/hypr/scripts/suspend.sh
+++ hyprland/.config/hypr/scripts/suspend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 swayidle -w \
-timeout 6000 ' hyprlock ' \
+timeout 100 ' hyprlock ' \
 #timeout 400 ' hyprctl dispatch dpms off' \
 #timeout 12000 'systemctl suspend' \
 #resume ' hyprctl dispatch dpms on' \
