86c0809f13c14a409407a1bc926e662d31b4cd3d
Stephen Eyring <accounts@eyring-s.de>
2025-02-23 12:16:17 +0100

    shell_config bugfix

--- shell_config/.config/shell/common/30_functions.sh
+++ shell_config/.config/shell/common/30_functions.sh
@@ -5,3 +5,27 @@ cdd() {
     cd "$1" && clear && eza -ahl --color=auto
 }
 
+#   Alias update -> updates package manager
+update() {
+  . /etc/os-release
+
+  case "$ID" in
+    ubuntu|debian)
+      sudo apt update && apt dist-upgrade -y
+      ;;
+    fedora)
+      sudo dnf update -y
+      ;;
+    centos|rhel)
+      sudo yum update -y
+      ;;
+    arch|manjaro)
+      sudo pacman -Syu -y --noconfirm
+      ;;
+    opensuse-tumbleweed)
+      sudo zypper up -y
+      ;;
+    *)
+      ;;
+  esac
+}
--- shell_config/.zshrc
+++ shell_config/.zshrc
@@ -8,12 +8,11 @@
 #-------------------------------------------
 #-----   Load config   -----
 
-CONFIG_DIR="$HOME/.config/shell"
+export CONFIG_DIR="$HOME/.config/shell"
 
 load_shell_config () {
   local SHELL_DIR="$CONFIG_DIR/$(basename "$SHELL")"
-  for file in "$CONFIG_DIR/common/"*.sh "$SHELL_DIR/"*.sh;
-  do
+  for file in "$CONFIG_DIR/common/"*.sh "$SHELL_DIR/"*.sh"; do
     [ -f "$file" ] && source "$file"
   done
 }
