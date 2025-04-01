45297bb398602f790eb73c77140ca979a54e3e60
Stephen Eyring <accounts@eyring-s.de>
2025-04-01 17:31:31 +0200

    maus

--- shell_config/.config/shell/common/20_env.sh
+++ shell_config/.config/shell/common/20_env.sh
@@ -3,6 +3,9 @@
 # Env
 export M_HEADSET="68:59:32:0B:6B:59"
 export M_BT_BOX="04:21:44:72:B1:08"
+export LOG_MOUSE_T440P="D3:31:C1:A3:C1:5B"
+export LOG_MOUSE_X1CARB="D3:31:C1:A3:C1:5C"
+
 
 # Programs
 export FETCH_CMD="neofetch" 
